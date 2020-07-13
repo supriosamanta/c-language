59d5d6058f42cfeb10e387d1893da21cfaf65c5d
supriosamanta <68184740+supriosamanta@users.noreply.github.com>
2020-07-13 17:17:48 +0530

    Add files via upload

---
+++ sum of first 3 numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int a,b,c,d;
+    printf("enter the value of three numbers\n");
+    scanf("%d %d %d",&a,&b,&c);
+    d=a+b+c;
+    printf("the sum of %d %d and %d is %d",a,b,c,d);
+    return 0;
+}
